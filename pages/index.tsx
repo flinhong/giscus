af81874a0d52293e5773e9641604d3680be56ba5
Frank Lin <me@linhong.eu.org>
2023-02-18 12:45:11 +0800

    update repo info

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,9 +144,9 @@ export default function Home({
         <div id="comments" className="giscus w-full my-8" />
         <Script
           src="/client.js"
-          data-repo="giscus/giscus"
-          data-repo-id="MDEwOlJlcG9zaXRvcnkzNTE5NTgwNTM="
-          data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzk2NTc1"
+          data-repo="flinhong/chaos"
+          data-repo-id="R_kgDOGK_u5g"
+          data-category-id="DIC_kwDOGK_u5s4B_UhX"
           data-mapping="specific"
           data-term="Welcome to giscus!"
           data-theme="preferred_color_scheme"
